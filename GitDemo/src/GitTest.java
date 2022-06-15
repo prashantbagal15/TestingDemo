6fe1d6a40e779d82c6e0c4e675f26c27ab6d4c80
AbhijitMadane27 <madane.abhijit27@gmail.com>
2022-06-15 11:24:39 +0530

    by

--- GitDemo/src/GitTest.java
+++ GitDemo/src/GitTest.java
@@ -2,10 +2,16 @@
 public class GitTest 
 {
 
+	public void m1()
+	{
+		System.out.println("cdfffd");
+	}
+	
+	
 	public static void main(String[] args)
 	{
 	
-		
+		System.out.println("Abhiiii");
 		
 		
 		
